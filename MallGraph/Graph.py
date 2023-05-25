6363209fe23bd52779331a1311dc300b73540bbf
1blackghost <ashishjosephnew@gmail.com>
2023-05-25 14:38:10 +0530

    finished floor 1

--- MallGraph/Graph.py
+++ MallGraph/Graph.py
@@ -33,6 +33,53 @@ class MakeGraph:
         self.mall.add_connection("Stairs", "Floor 1 RestRoom")
         self.mall.add_connection("Floor 1 RestRoom", "Data Center")
 
+        self.mall.add_connection("Entrance Hall", "Service Counter")
+        self.mall.add_connection("Service Counter", "Rest Area")
+        self.mall.add_connection("Rest Area", "Stairs")
+        self.mall.add_connection("Elevator", "Stairs")
+        self.mall.add_connection("Stairs", "HP")
+        self.mall.add_connection("HP", "Apple")
+        self.mall.add_connection("Apple", "Dell")
+        self.mall.add_connection("Dell", "Samsung")
+        self.mall.add_connection("Samsung", "Coridoor 1")
+        self.mall.add_connection("Coridoor 1", "Coridoor 2")
+
+        self.mall.add_connection("Dell", "Coridoor 1")
+        self.mall.add_connection("Coridoor 2", "MyG")
+        self.mall.add_connection("Coridoor 2", "Acer")
+        self.mall.add_connection("Stairs", "HP")
+
+        #working till here
+        self.mall.add_connection("Coridoor 3", "MyG")
+        self.mall.add_connection("Coridoor 3", "Rest Area")
+        self.mall.add_connection("Coridoor 2", "Chicking")
+        self.mall.add_connection("Rest Area", "Hug A Mug")
+        self.mall.add_connection("Service Counter", "KFC")
+        self.mall.add_connection("KFC", "Dominos")
+        self.mall.add_connection("KFC", "StarBucks")
+
+        self.mall.add_connection("Service Counter", "Dominos")
+        self.mall.add_connection("KFC", "McDonalds")
+        self.mall.add_connection("Dominos", "McDonalds")
+        self.mall.add_connection("StarBucks", "Chickos")
+        self.mall.add_connection("McDonalds", "Chickos")
+        self.mall.add_connection("Coridoor 4", "StarBucks")
+        self.mall.add_connection("Coridoor 4", "Chickos")
+        self.mall.add_connection("Coridoor 5", "Coridoor 4")
+        self.mall.add_connection("Coridoor 5", "Acer")
+        self.mall.add_connection("Coridoor 5", "Chicking")
+        self.mall.add_connection("Coridoor 3", "Stairs")
+
+
+
+
+
+
+
+
+
+
+
     def find_path(self, start, destination):
         shortest_path = self.mall.find_shortest_path(start, destination)
         return shortest_path
